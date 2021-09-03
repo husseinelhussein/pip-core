0e52ac4908795ca3bf08a4648482a5109eae26a7
Hussein <hussein@devvly.com>
2021-09-03 16:56:49 +0300

    build configuration

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,11 +1,12 @@
 import * as express from "express"
+import {ParsedQs} from "qs";
 declare global {
     namespace Express {
         interface Request {
             context: {
                 id: number | string;
             };
-            query: {
+            query: ParsedQs | {
                 [key: string]: any
             };
         }
